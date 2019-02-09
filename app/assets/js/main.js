27920c1c1d5c181882e6cb5372958866463cfb0d
Jesse Bees <jesse@toomanybees.com>
2019-02-09 00:01:05 -0500

    No parallax, ok

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -19,76 +19,76 @@
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
 	 */
-	$.fn._parallax = (skel.vars.browser == 'ie' || skel.vars.browser == 'edge' || skel.vars.mobile) ? function() { return $(this) } : function(intensity) {
+	// $.fn._parallax = (skel.vars.browser == 'ie' || skel.vars.browser == 'edge' || skel.vars.mobile) ? function() { return $(this) } : function(intensity) {
 
-		var	$window = $(window),
-			$this = $(this);
+	// 	var	$window = $(window),
+	// 		$this = $(this);
 
-		if (this.length == 0 || intensity === 0)
-			return $this;
+	// 	if (this.length == 0 || intensity === 0)
+	// 		return $this;
 
-		if (this.length > 1) {
+	// 	if (this.length > 1) {
 
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
+	// 		for (var i=0; i < this.length; i++)
+	// 			$(this[i])._parallax(intensity);
 
-			return $this;
+	// 		return $this;
 
-		}
+	// 	}
 
-		if (!intensity)
-			intensity = 0.25;
+	// 	if (!intensity)
+	// 		intensity = 0.25;
 
-		$this.each(function() {
+	// 	$this.each(function() {
 
-			var $t = $(this),
-				on, off;
+	// 		var $t = $(this),
+	// 			on, off;
 
-			on = function() {
+	// 		on = function() {
 
-				$t.css('background-position', 'center 100%, center 100%, center 0px');
+	// 			$t.css('background-position', 'center 100%, center 100%, center 0px');
 
-				$window
-					.on('scroll._parallax', function() {
+	// 			$window
+	// 				.on('scroll._parallax', function() {
 
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+	// 					var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
+	// 					$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
 
-					});
+	// 				});
 
-			};
+	// 		};
 
-			off = function() {
+	// 		off = function() {
 
-				$t
-					.css('background-position', '');
+	// 			$t
+	// 				.css('background-position', '');
 
-				$window
-					.off('scroll._parallax');
+	// 			$window
+	// 				.off('scroll._parallax');
 
-			};
+	// 		};
 
-			skel.on('change', function() {
+	// 		skel.on('change', function() {
 
-				if (skel.breakpoint('medium').active)
-					(off)();
-				else
-					(on)();
+	// 			if (skel.breakpoint('medium').active)
+	// 				(off)();
+	// 			else
+	// 				(on)();
 
-			});
+	// 		});
 
-		});
+	// 	});
 
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
-			});
+	// 	$window
+	// 		.off('load._parallax resize._parallax')
+	// 		.on('load._parallax resize._parallax', function() {
+	// 			$window.trigger('scroll');
+	// 		});
 
-		return $(this);
+	// 	return $(this);
 
-	};
+	// };
 
 	$(function() {
 
@@ -231,7 +231,7 @@
 					$image = $this.find('.image'), $img = $image.find('img');
 
 				// Parallax.
-					$this._parallax(0.275);
+					// $this._parallax(0.275);
 
 				// Image.
 					if ($image.length > 0) {
